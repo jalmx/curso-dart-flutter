15d46d39ce6315b3f42d1379558b731273591a1a
jalmx <jalm_x@hotmail.com>
2024-01-05 16:06:45 -0600

    add: if

---
+++ docs/programs/if.dart
@@ -0,0 +1,18 @@
+void main() {
+  int valor1 = 4;
+  int valor2 = 7;
+
+  if (valor1 < valor2) {
+    print("$valor1 < $valor2");
+  } else if (valor1 > valor2) {
+    print("$valor1 > $valor2");
+  } else if (valor1 >= valor2) {
+    print("$valor1 >= $valor2");
+  } else if (valor1 <= valor2) {
+    print("$valor1 <= $valor2");
+  } else if (valor1 == valor2) {
+    print("$valor1 == $valor2");
+  } else if (valor1 != valor2) {
+    print("$valor1 != $valor2");
+  }
+}
---
+++ docs/programs/logico.dart
@@ -0,0 +1,7 @@
+main() {
+  bool valor1 = true;
+  bool valor2 = false;
+
+  print("${valor1 && valor2}");
+  print("${valor2 || valor1}");
+}
